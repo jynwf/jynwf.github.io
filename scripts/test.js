e7f0153ab824b55225f03ffe14efc618ba7c5972
jynwf <68534940+jynwf@users.noreply.github.com>
2020-07-20 17:25:30 +0800

    first commit

---
+++ scripts/test.js
@@ -0,0 +1,35 @@
+console.log("Hello World");
+
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+let num1 = 1;
+
+console.log(num1+1);
+
+let array = ["a","bcd","c"];
+
+
+let num = 5
+if( num > 2) {
+    console.log("hihi");
+    alert('hello!')    
+} else if (num === 5 ) {
+    console.log("hi");
+} else {
+    console.log('fuck')
+}
+
+
+function sayhello() {
+    alert("hello")
+}
+
+
+
+
+console.log(array[1]);
+
+let mybutton = document.querySelector('.btn1')
+mybutton.onclick = sayhello();
